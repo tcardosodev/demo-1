a320923fa892b19090bd5da368300ac840032c4e
tcardoso <tcardoso@microsoft.com>
2021-11-23 22:51:22 -0300

    added slack msg

--- notify.sh
+++ notify.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' $SLACK_SECRET
